120241b2e26f267e8ca66c4ef9c61fc013e74fe5
alisoneoz <alisonestephanyoz@gmail.com>
2023-11-10 11:18:43 -0400

    Install Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './globals.css'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div className='text-3xl font-bold underline'>
+            Hello InstaSnapGram!
+        </div>
+    )
 }
 
 export default App
